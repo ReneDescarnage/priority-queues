ed0ffaee4247ff367c69584d69aa6b398130074b
Simone Stefani <s.stefani95@gmail.com>
2018-01-13 11:32:55 +0100

    Basic linked list working

--- doubly-linked-list/doubly_linked_list_pq.c
+++ doubly-linked-list/doubly_linked_list_pq.c
@@ -1,55 +1,133 @@
-#include<stdio.h>
-#include<stdlib.h>
+// #include<stdio.h>
+// #include<stdlib.h>
 
-struct node {
-    struct node *next;
-    struct node *previous;
-    int data;
-    int priority;
+// struct node {
+//     struct node *next;
+//     struct node *previous;
+//     double priority;
+// };
+
+// typedef struct node Node;
+
+// Node *head = NULL;
+// Node *tail = NULL;
+
+// // // void enqueue() {
+
+// // // }
+
+// // // struct node* dequeue() {
+
+// // // }
+
+// void insert_at_head(Node *head, Node *tail, double priority) {
+//     printf("ins\n");
+
+//     struct node *newNode = (struct node*) malloc(sizeof(struct node));
+//     newNode->priority = priority;
+
+//     if (head == NULL) {
+//         tail = newNode;
+//     } else {
+//         head->previous = newNode;
+//     }
+
+//     newNode->next = head;
+//     head = newNode;
+
+// }
+
+// // void insert_at_tail(Node *head, Node *tail, double priority) {
+    
+// //     struct node *newNode = (struct node*) malloc(sizeof(struct node));
+// //     newNode->priority = priority;
+
+// //     if (head == NULL) {
+// //         tail = newNode;
+// //     } else {
+// //         tail->next = newNode;
+// //         newNode->previous = tail;
+// //     }
+
+// //     tail = newNode;
+
+// // }
+
+// void traverse(Node *head) {
+//     printf("trav %s\n", (char *)&head);
+//     struct node *temp = head;
+//     // printf("node: %.f\n", temp->priority);
+//     while(temp->next != NULL) {
+//         // printf("node: %.f\n", temp->priority);
+//         temp = temp->next;
+//     }
+// }
+
+// int main() {
+//     printf("main\n");
+//     insert_at_head(head, tail, 1.0);
+//     insert_at_head(head, tail, 2.0);
+//     traverse(head);
+// }
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node  {
+	double priority;
+	struct node* next;
+	struct node* previous;
 };
 
-struct node *head = NULL;
-struct node *tail = NULL;
+struct node* head;
 
-void enqueue() {
-
+struct node* create_new_node(double priority) {
+	struct node* newNode = (struct node*)malloc(sizeof(struct node));
+	newNode->priority = priority;
+	newNode->previous = NULL;
+	newNode->next = NULL;
+	return newNode;
 }
 
-struct node* dequeue() {
-
+void prepend(double priority) {
+	struct node* newNode = create_new_node(priority);
+	if (head == NULL) {
+		head = newNode;
+		return;
+	}
+	head->previous = newNode;
+	newNode->next = head; 
+	head = newNode;
 }
 
-void insert_at_head(int data, int priority) {
-
-    struct node *newNode = (struct node*) malloc(sizeof(struct node));
-    newNode->data = data;
-    newNode->priority = priority;
-
-    if (head == NULL) {
-        tail = newNode;
-    } else {
-        head->previous = newNode;
-    }
-
-    newNode->next = head;
-    head = newNode;
-
+void append(double priority) {
+	struct node* temp = head;
+	struct node* newNode = create_new_node(priority);
+	if (head == NULL) {
+		head = newNode;
+		return;
+	}
+	while(temp->next != NULL) temp = temp->next;
+	temp->next = newNode;
+	newNode->previous = temp;
 }
 
-void insert_at_tail(int data, int priority) {
-    
-    struct node *newNode = (struct node*) malloc(sizeof(struct node));
-    newNode->data = data;
-    newNode->priority = priority;
-
-    if (head == NULL) {
-        tail = newNode;
-    } else {
-        tail->next = newNode;
-        newNode->previous = tail;
-    }
-
-    tail = newNode;
-
+void traversal_print() {
+	struct node* temp = head;
+	printf("List: ");
+	while (temp != NULL) {
+		printf("%f ", temp->priority);
+		temp = temp->next;
+	}
+	printf("\n");
 }
 
+int main() {
+
+	head = NULL;
+	
+    prepend(1.0);
+    prepend(2.0);
+    traversal_print();
+	
+}
---
+++ doubly-linked-list/test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <doubly_linked_list_pq.h>
+
+void main() {
+
+}
