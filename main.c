11cba4731d965d6e224667a7a0f4e086c5e1964c
cedric <cedric.seger.1@gmail.com>
2018-01-13 18:07:53 +0100

    Add argument parsing for main

--- main.c
+++ main.c
@@ -4,18 +4,71 @@
 #include "input.h"
 #include "./doubly-linked-list/doubly_linked_list_pq.h"
 
-// Declerations
-void run_tests(int queue_size, char hold, char* dist_filepath, int num_ops, int num_trials, char* save_name);
+#include <ctype.h>
+#include <unistd.h>
 
-int main(void) {
-	char* dist_filepath = "samples/triangular.txt" ;
-	char* save_filepath = "samples/triangular_exec" ;
-	char hold = 1
-	run_tests(50,hold,dist_filepath,1000,5,save_filepath);
+// Declerations
+void run_tests(int queue_size, int hold, char* dist_filepath, int num_ops, int num_trials, char* save_name);
+
+int main(int argc, char *argv[]) {
+	int queue_size = 0;
+	int hold = 1;
+	char* dist_filepath = NULL;
+	char* save_filepath = NULL;
+	int num_ops = 0;
+	int num_trials = 0;
+
+	int index;
+	int c;
+
+	while ((c = getopt (argc, argv, "q:h:d:s:o:t:")) != -1) {
+		switch (c) {
+			case 'q':
+				queue_size = atoi(optarg);
+				break;
+			case 'h':
+				hold = atoi(optarg);
+				break;
+			case 'd':
+				dist_filepath = optarg;
+				break;
+			case 's':
+				save_filepath = optarg;
+				break;
+			case 'o':
+				num_ops = atoi(optarg);
+				break;
+			case 't':
+				num_trials = atoi(optarg);
+				break;
+			case '?':
+				if (isprint (optopt))
+					fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+				else
+					fprintf (stderr,
+		               "Unknown option character `\\x%x'.\n",
+		               optopt);
+				return 1;
+			default:
+				abort();
+		}
+	}
+
+	printf("Running experiment with following options:\n");
+	printf ("queue_size = %d, hold = %d, dist_filepath = %s, save_filepath = %s, num_ops = %d, num_trials = %d\n",
+	  queue_size, hold, dist_filepath, save_filepath, num_ops, num_trials);
+	printf("--------->\n");
+
+	for (index = optind; index < argc; index++)
+		printf ("Non-option argument %s\n", argv[index]);
+
+
+	run_tests(queue_size,hold,dist_filepath,num_ops,num_trials,save_filepath);
+	return 0;
 }
 
 // Add argument for data structure to use??
-void run_tests(int queue_size, char hold, char* dist_filepath, int num_ops, int num_trials, char* save_name) {
+void run_tests(int queue_size, int hold, char* dist_filepath, int num_ops, int num_trials, char* save_name) {
 	clock_t start, end;
 	double exec_time;
 	double execution_times[num_trials];
