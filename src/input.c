30695fa62c2dd5a88f08ffc23ca0de7e8de09dfb
Simone Stefani <s.stefani95@gmail.com>
2018-01-14 19:40:28 +0100

    Cleanup main and input

--- src/input.c
+++ src/input.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <string.h>
 
 #define N_SAMPLES 10000
 #define CHAR_TO_READ 255
@@ -32,36 +32,36 @@
 
 // } // END OF MAIN
 
-void read_data(char* filepath, double* data_array, int num_to_read) {
+void read_data(char *filepath, double *data_array, int num_to_read) {
   char buff[CHAR_TO_READ];
   char *end;
   double num;
   FILE *fp;
 
   fp = fopen(filepath, "r");
-  if (fp == NULL)
-      {
-      fprintf(stderr,"Error opening file.\n");
-      exit(2);
-      }
+  if (fp == NULL) {
+    fprintf(stderr, "Error opening file.\n");
+    exit(2);
+  }
 
   // Read 'num_to_read' amount of data into our data structure on stack
   int i;
-  for (i=0; i<num_to_read; i++) {
-    // Reads a line from the file byte by byte...Max CHAR_TO_READ bytes in a single line
-    // Regard it as a null terminated string
-    //If null, then we have reached the EOF. So lets break.
+  for (i = 0; i < num_to_read; i++) {
+    // Reads a line from the file byte by byte...Max CHAR_TO_READ bytes in a
+    // single line Regard it as a null terminated string
+    // If null, then we have reached the EOF. So lets break.
     if (fgets(buff, CHAR_TO_READ, fp) == NULL) {
-      fprintf(stderr,"Error reading enough data. EOF reached prematurely.\n");
+      fprintf(stderr, "Error reading enough data. EOF reached prematurely.\n");
       break;
     }
     // Else lets convert into double and store into data array
     num = strtod(buff, &end);
     data_array[i] = num;
 
-    // Expect character after number to be the newline character...Otherwise error.
-    if (*end != '\n'){
-      fprintf(stderr,"Error converting string to double.\n");
+    // Expect character after number to be the newline character...
+    // Otherwise error.
+    if (*end != '\n') {
+      fprintf(stderr, "Error converting string to double.\n");
       printf("Char after num is: %s\n", end);
       exit(2);
     }
@@ -70,17 +70,17 @@ void read_data(char* filepath, double* data_array, int num_to_read) {
   fclose(fp);
 }
 
-// Function to save data to a file
-void save_data(char* name, double* data_array, int size){
-  // triangular_exec"
+// Function to save data to a file.
+void save_data(char *name, double *data_array, int size) {
 
   printf("Saving data to file...\n");
-  //Open file
+  // Open file
   FILE *fp;
-  
-  // Concatenate strings...Consider removing??
-  char *filepath = malloc(strlen(name)+strlen(".csv")+1);//+1 for the null-terminator
-  //in real code you would check for errors in malloc here
+
+  // Concatenate strings.
+  char *filepath =
+      malloc(strlen(name) + strlen(".csv") + 1); //+1 for the null-terminator
+  // in real code you would check for errors in malloc here
   strcpy(filepath, name);
   strcat(filepath, ".csv");
 
@@ -88,12 +88,12 @@ void save_data(char* name, double* data_array, int size){
   int i;
 
   if (fp == NULL) {
-    fprintf(stderr,"Can't open output file: %s.\n", filepath);
+    fprintf(stderr, "Can't open output file: %s.\n", filepath);
     exit(2);
   }
 
-  // //Save data to file
-  for(i=0; i<size; i++){
+  // Save data to file.
+  for (i = 0; i < size; i++) {
     fprintf(fp, ",%f", data_array[i]);
   }
   fclose(fp);
